a5e0303087de0fa7757d50d193d520d3693c8869
marimelon <29140293+marimelon@users.noreply.github.com>
2021-12-13 15:45:50 +0900

    update

--- craftpy/__init__.py
+++ craftpy/__init__.py
@@ -1,7 +1,6 @@
 from . import auto_base
 from . import crafter_base
 from . import finisher_pattern
-# from . import ingame_crafter
 from . import simcrafter
 from . import utils
 from . import craftcpp
